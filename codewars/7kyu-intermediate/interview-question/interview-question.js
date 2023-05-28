45e17e0ed1fa87d87372b6c846e3d3abf6686a92
AccRPA <accrpaw10@gmail.com>
2023-05-29 00:27:27 +0200

    Create interview-question.js

---
+++ codewars/7kyu-intermediate/interview-question/interview-question.js
@@ -0,0 +1,17 @@
+ function getStrings(city){
+   const regExp = new RegExp('[A-Za-z]', 'gi');
+   const lettersArray = city.toLowerCase().match(regExp);
+   
+   const objAccumulator = {};
+   lettersArray.reduce((accumulator, letter) => {
+      let value = '*';
+      if (letter in accumulator){
+        value += accumulator[letter];
+      }
+      accumulator[letter] = value;
+      return accumulator;
+   }, objAccumulator);
+   
+   const outArray = Object.keys(objAccumulator).map(x => `${x}:${objAccumulator[x]}`);
+   return outArray.join(',');
+}
