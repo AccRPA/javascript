93cfd18d96b0740185bb5710ec9ffb15e28d64c3
AccRPA <accrpaw10@gmail.com>
2023-08-17 18:58:00 +0200

    Type conversion and comparison

--- Exercises/01. Basics/08_Basics_Types.js
+++ Exercises/01. Basics/08_Basics_Types.js
@@ -7,7 +7,8 @@ console.log(1 + 2 + '3' + 4);
 console.log(1 < 2 < 3);
 console.log(2 < 1 == 0); // => 2 < 1 = false; Number(false) === 0 = true
 console.log(null == 0); // false: because null is not equal to any other types except undefined
-console.log(null <= 0); // true: because the greater/lesser comparison converts to Number
+console.log(null <= 0); // true: because the comparison converts to Number since null type is different than 0 type
+console.log(typeof null);
 //Number(undefined) => Nan
 //Number(null) => 0
 
@@ -18,3 +19,10 @@ console.log(null <= 0); // true: because the greater/lesser comparison converts 
 // Conversion to String: when printing or with the + operator
 // Conversion to Number: for math or expresions
 // Conversion to Boolean: Logic operations
+
+console.log(undefined < 0);
+// NaN es un valor numérico especial que retorna falso para todas las comparaciones.
+
+console.log(Number([])); // 0
+console.log(Number({})); // NaN
+// https://quizzesforyou.com/quizzes/javascript
