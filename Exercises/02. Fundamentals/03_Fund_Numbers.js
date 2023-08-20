29ade9c51170eafd08d966e6bf6e21fdd18c2063
AccRPA <accrpaw10@gmail.com>
2023-08-20 20:21:53 +0200

    Flip card

---
+++ Exercises/02. Fundamentals/03_Fund_Numbers.js
@@ -0,0 +1,7 @@
+const num1 = 1_00_0;
+const num2 = 1.23e6;
+const num3 = 1000;
+const num4 = 1234e-2; // 1234 / 100 => 12.34
+
+const num6 = 0xffffff // color white to number => #ffffff
+console.log(num6);
