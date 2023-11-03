9161a010cd987229e59ec814a532c88922f1d5f1
AccRPA <accrpaw10@gmail.com>
2023-11-03 23:42:13 +0100

    Toilet paper animation

---
+++ Tests/toilet-paper/script.js
@@ -0,0 +1,8 @@
+const rollElement = document.querySelector('.roll');
+const separationElement = document.querySelector('.separation');
+const paperElement = document.querySelector('.paper');
+
+rollElement.addEventListener('click', function(){
+    rollElement.classList.toggle('move-roll');
+    separationElement.classList.toggle('move-separation');
+});
