0d8f4056346787ce4e26f39bf09a2242c1240661
AccRPA <accrpaw10@gmail.com>
2023-11-04 09:56:10 +0100

    Toilet paper: separation animation solved

--- Tests/toilet-paper/script.js
+++ Tests/toilet-paper/script.js
@@ -5,4 +5,5 @@ const paperElement = document.querySelector('.paper');
 rollElement.addEventListener('click', function(){
     rollElement.classList.toggle('move-roll');
     separationElement.classList.toggle('move-separation');
+    paperElement.classList.toggle('move-paper');
 });
